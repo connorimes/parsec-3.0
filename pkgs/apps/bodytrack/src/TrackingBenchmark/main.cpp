451f88e7107356204f6dbf822dfaca610aa16cb7
Connor Imes <connor.k.imes@gmail.com>
2016-05-26 12:39:24 -0500

    Initial copper (bodytrack only)

--- pkgs/apps/bodytrack/src/TrackingBenchmark/main.cpp
+++ pkgs/apps/bodytrack/src/TrackingBenchmark/main.cpp
@@ -68,83 +68,105 @@ using namespace tbb;
 #define HB_ENERGY_IMPL
 #include <heartbeats/hb-energy.h>
 #include <heartbeats/heartbeat-accuracy-power.h>
-#include <poet/poet.h>
-#include <poet/poet_config.h>
+#include <raplcap.h>
+#include <copper.h>
+#include <copper-util.h>
 
 #define PREFIX "BODYTRACK"
-#define USE_POET // Power and performance control
 
 heartbeat_t* heart;
-poet_state* state;
-static poet_control_state_t* control_states;
-static poet_cpu_state_t* cpu_states;
+raplcap rc;
+copper* cop;
 
 using namespace std;
 
-static inline void hb_poet_init() {
-    float min_heartrate;
-    float max_heartrate;
-    int window_size;
-    double power_target;
-    unsigned int nstates;
-
-    if(getenv(PREFIX"_MIN_HEART_RATE") == NULL) {
-      min_heartrate = 0.0;
-    } else {
-      min_heartrate = atof(getenv(PREFIX"_MIN_HEART_RATE"));
+static int apply_powercap(double powercap) {
+  uint32_t n = raplcap_get_num_sockets(&rc);
+  if (n == 0) {
+    perror("raplcap_get_num_sockets");
+    return -1;
+  }
+  printf("Requested power cap %f for %"PRIu32" sockets\n", powercap, n);
+  uint32_t i;
+  raplcap_limit rl;
+  for (i = 0; i < n; i++) {
+    // getting this just for the timing part which has to be written back
+    if (raplcap_get_limits_package(i, &rc, NULL, &rl)) {
+      perror("raplcap_get_limits_package");
+      return -1;
     }
-    if(getenv(PREFIX"_MAX_HEART_RATE") == NULL) {
-      max_heartrate = 100.0;
-    } else {
-      max_heartrate = atof(getenv(PREFIX"_MAX_HEART_RATE"));
+    printf("Old RAPL config for socket %"PRIu32": time=%f power=%f\n", i, rl.seconds, rl.watts);
+    // share powercap evenly across sockets
+    rl.watts = powercap / (double) n;
+    printf("New RAPL config for socket %"PRIu32": time=%f power=%f\n", i, rl.seconds, rl.watts);
+    if (raplcap_set_limits_package(i, &rc, NULL, &rl)) {
+      perror("raplcap_set_limits_package");
+      return -1;
     }
-    if(getenv(PREFIX"_WINDOW_SIZE") == NULL) {
-      window_size = 30;
-    } else {
-      window_size = atoi(getenv(PREFIX"_WINDOW_SIZE"));
-    }
-    if(getenv(PREFIX"_POWER_TARGET") == NULL) {
-      power_target = 70;
-    } else {
-      power_target = atof(getenv(PREFIX"_POWER_TARGET"));
-    }
-
-    printf("init heartbeat with %f %f %d\n", min_heartrate, max_heartrate, window_size);
-    heart = heartbeat_acc_pow_init(window_size, 100, "heartbeat.log",
-                                   min_heartrate, max_heartrate,
-                                   0, 100,
-                                   1, hb_energy_impl_alloc(), power_target, power_target);
-    if (heart == NULL) {
-      fprintf(stderr, "Failed to init heartbeat.\n");
-      exit(1);
-    }
-#ifdef USE_POET
-    if (get_control_states(NULL, &control_states, &nstates)) {
-      fprintf(stderr, "Failed to load control states.\n");
-      exit(1);
-    }
-    if (get_cpu_states(NULL, &cpu_states, &nstates)) {
-      fprintf(stderr, "Failed to load cpu states.\n");
-      exit(1);
-    }
-    state = poet_init(heart, nstates, control_states, cpu_states, &apply_cpu_config, &get_current_cpu_state, 1, "poet.log");
-    if (state == NULL) {
-      fprintf(stderr, "Failed to init poet.\n");
-      exit(1);
-    }
-#endif
-   printf("heartbeat init'd\n");
-
+  }
+  return 0;
 }
 
-static inline void hb_poet_finish() {
-#ifdef USE_POET
-    poet_destroy(state);
-    free(control_states);
-    free(cpu_states);
-#endif
-    heartbeat_finish(heart);
-    printf("heartbeat finished\n");
+static inline void hb_copper_init() {
+  double min_heartrate = 0.0;
+  double max_heartrate = 100.0;
+  int window_size = 20;
+  double min_power = 0.0;
+  double max_power = 100.0;
+  const char* model = NULL;
+
+  if (getenv(PREFIX"_MIN_HEART_RATE") != NULL) {
+    min_heartrate = atof(getenv(PREFIX"_MIN_HEART_RATE"));
+  }
+  if (getenv(PREFIX"_MAX_HEART_RATE") != NULL) {
+    max_heartrate = atof(getenv(PREFIX"_MAX_HEART_RATE"));
+  }
+  if (getenv(PREFIX"_WINDOW_SIZE") != NULL) {
+    window_size = atoi(getenv(PREFIX"_WINDOW_SIZE"));
+  }
+  if (getenv(PREFIX"_MIN_POWER") != NULL) {
+    min_power = atof(getenv(PREFIX"_MIN_POWER"));
+  }
+  if (getenv(PREFIX"_MAX_POWER") != NULL) {
+    max_power = atof(getenv(PREFIX"_MAX_POWER"));
+  }
+  model = getenv(PREFIX"_MODEL");
+
+  printf("init heartbeat with %f %f %f %f %d\n", min_heartrate, max_heartrate, min_power, max_power, window_size);
+  heart = heartbeat_acc_pow_init(window_size, 100, "heartbeat.log",
+                                 min_heartrate, max_heartrate,
+                                 0, 100,
+                                 1, hb_energy_impl_alloc(), min_power, max_power);
+  if (heart == NULL) {
+    fprintf(stderr, "Failed to init heartbeat.\n");
+    exit(1);
+  }
+  printf("heartbeat init'd\n");
+  if (raplcap_init(&rc)) {
+    perror("raplcap_init");
+    exit(1);
+  }
+  // start at max power
+  if (apply_powercap(max_power)) {
+    perror("apply_powercap");
+    exit(1);
+  }
+  printf("raplcap init'd\n");
+  cop = copper_alloc_init((min_heartrate + max_heartrate) / 2.0, min_power, max_power, max_power, 1, "copper.log", model);
+  if (cop == NULL) {
+    perror("copper_alloc_init");
+    exit(1);
+  }
+  printf("copper init'd\n");
+}
+
+static inline void hb_copper_finish() {
+  copper_destroy_free(cop);
+  printf("copper destroyed\n");
+  raplcap_destroy(&rc);
+  printf("raplcap destroyed\n");
+  heartbeat_finish(heart);
+  printf("heartbeat finished\n");
 }
 
 //templated conversion from string
@@ -270,10 +292,6 @@ int mainOMP(string path, int cameras, int frames, int particles, int layers, int
 #endif
 	for(int i = 0; i < frames; i++)														//process each set of frames
 	{
-		heartbeat_acc(heart, i, 1.0);
-#if defined(USE_POET)
-		poet_apply_control(state);
-#endif
 		cout << "Processing frame " << i << endl;
 		if(!pf.Update((float)i))														//Run particle filter step
 		{	cout << "Error loading observation data" << endl;
@@ -337,9 +355,14 @@ int mainPthreads(string path, int cameras, int frames, int particles, int layers
 	for(int i = 0; i < frames; i++)														//process each set of frames
 	{
 		heartbeat_acc(heart, i, 1.0);
-#if defined(USE_POET)
-		poet_apply_control(state);
-#endif
+                if (i != 0 && i % hb_get_window_size(heart) == 0) {
+                    double powercap = copper_adapt(cop, i, hb_get_windowed_rate(heart));
+                    if (powercap <= 0) {
+                        perror("copper_adapt");
+                    } else {
+                        apply_powercap(powercap);
+                    }
+                }
 		cout << "Processing frame " << i << endl;
 		if(!pf.Update((float)i))														//Run particle filter step
 		{	cout << "Error loading observation data" << endl;
@@ -438,10 +461,6 @@ int mainSingleThread(string path, int cameras, int frames, int particles, int la
 #endif
 	for(int i = 0; i < frames; i++)														//process each set of frames
 	{
-		heartbeat_acc(heart, i, 1.0);
-#if defined(USE_POET)
-		poet_apply_control(state);
-#endif
 		cout << "Processing frame " << i << endl;
 		if(!pf.Update((float)i))														//Run particle filter step
 		{	cout << "Error loading observation data" << endl;
@@ -465,7 +484,7 @@ int main(int argc, char **argv)
 	bool OutputBMP;
 	int cameras, frames, particles, layers, threads, threadModel;								//process command line parameters to get path, cameras, and frames
 
-	hb_poet_init();
+	hb_copper_init();
 
 #ifdef PARSEC_VERSION
 #define __PARSEC_STRING(x) #x
@@ -545,7 +564,7 @@ int main(int argc, char **argv)
 			break;
 	}
 
-	hb_poet_finish();
+	hb_copper_finish();
 
 #if defined(ENABLE_PARSEC_HOOKS)
         __parsec_bench_end();
